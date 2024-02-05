22cef78e6edf544a4650fb74e2ff4b84ba0e6325
Mohamed Kamal <ku18mz@gmail.com>
2024-02-05 06:37:51 +0200

    documentation

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 
 class square():
+    """Square class"""
     width = 0
     height = 0
 
     def __init__(self, *args, **kwargs):
+        """Constructor of the class"""
         for key, value in kwargs.items():
             setattr(self, key, value)
 
@@ -13,9 +15,11 @@ class square():
         return self.width * self.width
 
     def PermiterOfMySquare(self):
+        """Permiter of the square"""
         return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
+        """String representation of the square object"""
         return "{}/{}".format(self.width, self.height)
 
 
